5c27a38d6b035ec452c74a9292f827411128be70
Marc Gracia <mgbuddy@gmail.com>
2013-01-16 17:38:54 +0100

    Added reconnection logic. For now data to disconnected servers is lost

--- mirror.rb
+++ mirror.rb
@@ -2,38 +2,60 @@
 require 'socket'
 class Mirror 
 
+  def connector
+    wrongs = Array.new()
+    loop {
+      while(!@disconnected.empty?) do
+        connection=@disconnected.pop
+        if (connect(connection))
+          @connections << connection
+        else
+          wrongs << connection
+        end
+      end
+      wrongs.each do |connection| 
+        @disconnected << connection
+      end
+      wrongs=Array.new
+      sleep(@reconnect_interval)
+    }
+  end
+
   def initialize(hosts)
     @hosts=hosts
     @connections=Array.new
+    @disconnected = Array.new()
+    @reconnect_interval=20
     @hosts.each do |host|
        hostparams=host.split(':')
        connection={ :host => hostparams[0], :port => hostparams[1] }
-       @connections << connection
+       @disconnected << connection
+    end
+    @reconnector= Thread.new do
+      connector()
     end
   end
  
-  def connect_all() 
-    @connections.each do |connection|
-      connect(connection)
-    end
-  end
-  
   def connect(connection) 
     puts('Connecting to '+connection[:host]+":"+connection[:port].to_s)
     begin
       connection.store(:socket,TCPSocket.open(connection[:host],connection[:port]))
     rescue
       puts ("Coudn't connect to "+connection[:host])
+      return false
     end
+    return true
   end
 
   def send_data( data )
     @connections.each do |connection|
-        begin
-           connection[:socket].puts(data)
-        rescue 
-           puts("Socket closed to "+connection[:host]+" "+connection[:port].to_s)
-        end
+       begin
+         connection[:socket].puts(data)
+       rescue 
+         puts("Socket closed to "+connection[:host]+" "+connection[:port].to_s)
+         wrongs << connection
+         @disconnected.add(connection)
+       end
     end
   end
 end
